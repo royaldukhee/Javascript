cd067851acf1d8655601606f7bcf9fabee4ae61c
royaldukhee <sabinher444@gmail.com>
2023-12-16 20:13:02 +0545

    assignments

--- binaryToDecimal.js
+++ binaryToDecimal.js
@@ -1,14 +1,17 @@
 function binaryToDecimal(binary) {
-    while(binary>0){
-        var decimalNumber = 0;
-        var i = 0;
-        var result =binary%10;
-        console.log(result);
-        binary = Math.floor(binary/10);
-        decimalNumber = decimalNumber+result*2**i;
+    var decimalNumber = 0;
+    var i = 0;
+
+    while (binary > 0) {
+        var result = binary % 10;
+        binary = Math.floor(binary / 10);
+        decimalNumber += result * 2 ** i;
         i++;
     }
+
     return decimalNumber;
 }
-var Decimal = binaryToDecimal(1111);
-console.log(`the decimal number of ${binary} is ${Decimal}`);
+
+var binary = 101;
+var decimalResult = binaryToDecimal(binary);
+console.log(`The decimal number of ${binary} is ${decimalResult}`);
